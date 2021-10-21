c2c75c6010c168bb9dbebe2ee58572c18e67733d
Pawel Parcz <pawelparcz@gmail.com>
2021-10-21 16:52:42 +0200

    próbowałem 3 zad...

--- src/main/java/com/example/demo/Task.java
+++ src/main/java/com/example/demo/Task.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.example.demo.Group.G1;
+import static jdk.nashorn.internal.objects.NativeMath.max;
 
 public class Task {
 
@@ -28,10 +29,10 @@ public class Task {
 
     public static List<Person> getActivePlayersByScoreDesc(List<Person> people, Group group) {
         people = List.of(new Person("name", 2, group, true),
-        new Person("name", 1, group, true)
-    );
+                new Person("name", 1, group, true)
+        );
         return people.stream()
-                .filter(p->p.getTeam().equals(group)==true)
+                .filter(p -> p.getTeam().equals(group) == true)
                 .collect(Collectors.toList())
                 .stream()
                 .filter(p -> p.isActive() == true)
@@ -47,7 +48,11 @@ public class Task {
     Jeżeli ta liczba jest również równa, zwróć którąkolwiek z nich.
      */
     public static Group getGroupWithHighestScore(List<Person> people) {
-        throw new RuntimeException("Not Implemented!");
+        return people.stream()
+                .map(s->max(s.getScore())).findAny()
+                .get(getGroupWithHighestScore());
+
+
     }
 
   /*
